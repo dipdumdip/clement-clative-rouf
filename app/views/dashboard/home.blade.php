8d9b3b3dca71db3a005404ea29aecd50caaaa9cf
dipdumdip <tradebells123@gmail.com>
2014-09-05 15:53:49 +0100

    loved

--- app/views/dashboard/home.blade.php
+++ app/views/dashboard/home.blade.php
@@ -4,7 +4,60 @@
  <div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-xs-12 col-md-5">.col-xs-12 .col-sm-6 .col-md-8</div>
+        <div class="col-xs-12 col-md-5">
+        	<div class="row">
+        	    <div class="row">
+						  <div class="col-xs-12 col-sm-3 col-md-4 bg-warning">
+							<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 140px; height: 140px;" data-src="holder.js/140x140" class="img-thumbnail" alt="140x140">
+						  </div>
+
+					  <div class="col-xs-12 col-sm-9 col-md-8 bg-info">
+						<h3>Varun Jose</h3>
+						<address>
+						  795 Folsom Ave, Suite 600<br>
+						  San Francisco, CA 94107<br>
+						  <abbr title="Phone">P:</abbr> (123) 456-7890
+						</address>
+					  </div>
+				</div>
+        		<div class="row bg-warning">
+				  	<button type="button" class="btn btn-default btn-sm pull-right">
+					  <span class="glyphicon glyphicon-bell"></span> Events
+					</button>
+				  	
+				  	<button type="button" class="btn btn-default btn-sm pull-right">
+					  <span class="glyphicon glyphicon-envelope"></span> Mail
+					</button>
+				  	
+				  	<button type="button" class="btn btn-default btn-sm pull-right">
+					  <span class="glyphicon glyphicon-wrench"></span> Settings
+					</button>
+				  	
+				  	<button type="button" class="btn btn-default btn-sm pull-right">
+					  <span class="glyphicon glyphicon-star"></span> Live
+					</button>
+				</div>
+			</div>
+			<div class="row">
+				 <h4 class="bg-primary" style="padding:7px;width:auto;">Chair Invitation(s)</h4>
+				  <div class="row bg-warning" style="padding:3px 0;">
+				 	  <div class="col-xs-10 col-sm-10 col-md-9">chair requested from</div>
+					  <div class="col-xs-2 col-sm-2 col-md-3 ">
+						  <button type="button" class="btn btn-default btn-xs pull-right">No</button>
+						  <button type="button" class="btn btn-primary btn-xs pull-right">Ok</button>
+					  </div>
+				   </div>
+			</div>
+        	<div class="row">
+				<h4 class="bg-primary" style="padding:7px;width:auto;">Registered Companys</h4>
+				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 140px; height: 140px;" data-src="holder.js/140x140" class="img-thumbnail" alt="140x140">
+				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 140px; height: 140px;" data-src="holder.js/140x140" class="img-thumbnail" alt="140x140">
+				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 140px; height: 140px;" data-src="holder.js/140x140" class="img-thumbnail" alt="140x140">
+				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 140px; height: 140px;" data-src="holder.js/140x140" class="img-thumbnail" alt="140x140">
+				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 140px; height: 140px;" data-src="holder.js/140x140" class="img-thumbnail" alt="140x140">
+				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 140px; height: 140px;" data-src="holder.js/140x140" class="img-thumbnail" alt="140x140">
+        	</div>
+        </div>
         <div class="col-xs-12 col-md-7">
           <div id="dashboard_updates">
              <form role="form">
